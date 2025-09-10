580cb740d1954c97c8c6451504bdc3fef6cd174d
Sean C Davis <scdavis41@gmail.com>
2025-09-10 05:51:35 -0400

    Set up the basics for a new astro site

---
+++ astro/astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+import netlify from '@astrojs/netlify';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+      plugins: [tailwindcss()],
+    },
+
+  integrations: [react()],
+  adapter: netlify(),
+});
