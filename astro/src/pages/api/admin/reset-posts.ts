d3a82bdf7c40d05c9b8e5fcf8bccf221c49bd23a
Sean C Davis <scdavis41@gmail.com>
2025-09-10 06:14:28 -0400

    Reset endpoints

---
+++ astro/src/pages/api/admin/reset-posts.ts
@@ -0,0 +1,19 @@
+import { getStore } from '@netlify/blobs'
+import { clearPostsIndex } from '@utils/posts'
+import type { APIRoute } from 'astro'
+
+export const POST: APIRoute = async ({ request }) => {
+  const formData = await request.formData()
+  const apiKey = formData.get('api_key') as string
+
+  if (apiKey !== process.env.ADMIN_API_KEY) {
+    return new Response('Unauthorized', { status: 401 })
+  }
+
+  const postStore = getStore({ name: 'Post', consistency: 'strong' })
+  const allPostIds = (await postStore.list()).blobs.map(({ key }) => key)
+  await Promise.all(allPostIds.map((id) => postStore.delete(id)))
+  await clearPostsIndex()
+
+  return new Response('All posts deleted successfully')
+}
