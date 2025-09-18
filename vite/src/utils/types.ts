b7cb17450501f7df9535db4b97abbfc87b6f8d70
Sean C Davis <scdavis41@gmail.com>
2025-09-18 12:09:55 -0700

    Add admin seed function

---
+++ vite/src/utils/types.ts
@@ -0,0 +1,16 @@
+export type User = {
+  id: string;
+  username: string;
+  password: string;
+  avatarSrc: string;
+};
+
+export type Post = {
+  id: string;
+  title: string;
+  content: string;
+  userId: string;
+  createdAt: string;
+};
+
+export type PostWithUser = Post & { user: User };
