dd95be97e25c5574d00c8346bfd094504bcd4956
Sean C Davis <scdavis41@gmail.com>
2025-09-18 11:36:27 -0700

    Add header

---
+++ vite/src/components/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <header>
+      <div className="header-content container">
+        <a className="header-logo" href="/">
+          <img src="/img/blink-logo.svg" alt="BLINK Logo" />
+        </a>
+        <nav></nav>
+      </div>
+    </header>
+  );
+};
