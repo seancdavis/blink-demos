817f1d9bfe4210476a5fbba64c13dec1ecc0abb2
Sean C Davis <scdavis41@gmail.com>
2025-09-18 14:11:17 -0700

    remove vite-v2

--- vite-v2/utils/nl2br.mts
+++
@@ -1,6 +0,0 @@
-/**
- * Converts newlines in text to HTML line breaks
- */
-export function newlineToLineBreak(text: string): string {
-  return text.replace(/\r?\n/g, '<br>')
-}
--- vite-v2/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import netlify from '@netlify/vite-plugin'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  plugins: [react(), netlify()],
-  base: '/', // Ensure root-relative asset references
-})
